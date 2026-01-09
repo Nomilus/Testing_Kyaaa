5b001e278694a99a345f74ff8c19f96ba10c2ed8
67319093065-svg <67319093065@utc.ac.th>
2026-01-09 13:43:31 +0700

    Update main.py

--- main.py
+++ main.py
@@ -10,9 +10,6 @@ img = cv2.imread("../image/majo.png")
 # 3. ตรวจจับวัตถุ
 results = model(img)
 
-# 4. วาดผลลัพธ์ลงบนภาพ
-annotated_img = results[0].plot()
-
 # 5. แสดงผล
 cv2.imshow("YOLO Detection", annotated_img)
 cv2.waitKey(0)
@@ -20,3 +17,4 @@ cv2.destroyAllWindows()
 
 # 6. บันทึกภาพผลลัพธ์
 cv2.imwrite("result.jpg", annotated_img)
+
